82c2772e8dfacc64a02f356d4ce15fdea5588249
Art-1313 <71838986+Art-1313@users.noreply.github.com>
2021-05-05 15:09:51 +0300

    Add files via upload

--- queue_list/queue_list.cpp
+++ queue_list/queue_list.cpp
@@ -1,52 +1,7 @@
 ﻿#define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include "catch.hpp"
-#include <iostream>
-
-template <typename T> class Item
-{
-    T node; Item* next;
-public:
-    Item(const T& elem, Item* n = 0)
-    {
-        node = elem; next = n;
-    }
-
-    T& get_node()
-    {
-        return node;
-    }
-
-    Item*& get_next()
-    {
-        return next;
-    }
-};
-
-template <typename T> class Queue
-{
-    Item<T>* head, * tail; T rab;
-public:
-    Queue() { tail = head = 0; }
-
-    bool empty() { return head == 0; }
-
-    void put(const T& elem)
-    {
-        if (tail == 0) tail = head = new Item<T>(elem);
-        else tail = (tail->get_next() = new Item<T>(elem));
-    }
-
-    T& get()
-    {
-        Item<T>* p = head;
-        rab = head->get_node();
-        head = head->get_next();
-        delete p;
-        if (head == 0) tail = 0;
-        return rab;
-    }
-};
+#include "queue_list.h"
 
 TEST_CASE("Benchmark") {
     BENCHMARK("Plus 1000, minus 1000")
--- queue_list/queue_list.h
+++ queue_list/queue_list.h
@@ -1,8 +1,56 @@
 ﻿// queue_list.h : включаемый файл для стандартных системных включаемых файлов
 // или включаемые файлы для конкретного проекта.
 
-#pragma once
+#ifndef SORTING_QUEUE_LIST_H
+#define SORTING_QUEUE_LIST_H
 
 #include <iostream>
+
+template <typename T> class Item
+{
+    T node; Item* next;
+public:
+    Item(const T& elem, Item* n = 0)
+    {
+        node = elem; next = n;
+    }
+
+    T& get_node()
+    {
+        return node;
+    }
+
+    Item*& get_next()
+    {
+        return next;
+    }
+};
+
+template <typename T> class Queue
+{
+    Item<T>* head, * tail; T rab;
+public:
+    Queue() { tail = head = 0; }
+
+    bool empty() { return head == 0; }
+
+    void put(const T& elem)
+    {
+        if (tail == 0) tail = head = new Item<T>(elem);
+        else tail = (tail->get_next() = new Item<T>(elem));
+    }
+
+    T& get()
+    {
+        Item<T>* p = head;
+        rab = head->get_node();
+        head = head->get_next();
+        delete p;
+        if (head == 0) tail = 0;
+        return rab;
+    }
+};
+
+#endif //SORTING_QUEUE_LIST_H
 
 // TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
