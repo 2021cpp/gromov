ffd1743bc4f61b72e6ee10cd0cc3e2937a1e6314
Art-1313 <71838986+Art-1313@users.noreply.github.com>
2021-05-05 15:25:39 +0300

    Add files via upload

---
+++ queue_vector/queue_list.cpp
@@ -0,0 +1,43 @@
+﻿#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include "catch.hpp"
+#include "queue_list.h"
+
+TEST_CASE("Benchmark") {
+    BENCHMARK("Plus 1000, minus 1000")
+    {
+        Queue<int32_t> test;
+        for (size_t i = 0; i < 1000; ++i)
+        {
+            test.put(i);
+        }
+        for (size_t i = 0; i < 1000; ++i)
+        {
+            test.get();
+        }
+    };
+    BENCHMARK("Plus 100 000, minus 100 000")
+    {
+        Queue<int32_t> test;
+        for (size_t i = 0; i < 100000; ++i)
+        {
+            test.put(i);
+        }
+        for (size_t i = 0; i < 100000; ++i)
+        {
+            test.get();
+        }
+    };
+    BENCHMARK("Plus 10 000 000, minus 10 000 000")
+    {
+        Queue<int32_t> test;
+        for (size_t i = 0; i < 10000000; ++i)
+        {
+            test.put(i);
+        }
+        for (size_t i = 0; i < 10000000; ++i)
+        {
+            test.get();
+        }
+    };
+}
