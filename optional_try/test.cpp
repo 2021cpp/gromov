92f82535f7e1440e4185ace36f6603a58acb2c1f
Art-1313 <gromov.ak@phystech.edu>
2021-12-11 23:30:25 +0300

    add small changes

---
+++ optional_try/test.cpp
@@ -0,0 +1,7 @@
+#include "optional.hpp"
+#include "try.hpp"
+
+int main()
+{
+    //enter test code here
+}
--- optional_try/try.hpp
+++ optional_try/try.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <exception>
 
 template<class T>
